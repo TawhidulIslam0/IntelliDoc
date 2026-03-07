be686c49c7f127599fcdcf8b4289d0b885499b39
TawhidulIslam0 <islam100tawhidul@gmail.com>
2026-03-07 12:07:56 -0500

    feat: update authService.js with signup and login API functions

---
+++ frontend/src/api/authService.js
@@ -0,0 +1,46 @@
+ const API_BASE = "http://localhost:8000";
+
+// SIGNUP
+export const signupUser = async (username, email, password) => {
+  const response = await fetch(`${API_BASE}/api/users/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+    }),
+  });
+
+  if (!response.ok) {
+    const err = await response.json();
+    throw new Error(err.detail || "Signup failed");
+  }
+
+  return response.json();
+};
+
+
+// LOGIN 
+export const loginUser = async (username, password) => {
+  const formData = new FormData();
+
+  formData.append("username", username);
+  formData.append("password", password);
+
+  const response = await fetch(`${API_BASE}/api/users/login`, {
+    method: "POST",
+    body: formData,
+  });
+
+  if (!response.ok) {
+    const err = await response.json();
+    throw new Error(err.detail || "Login failed");
+  }
+
+  return response.json();
+};
+
+
