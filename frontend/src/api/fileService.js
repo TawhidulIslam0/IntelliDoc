17068f5684af435d3711c2bbeba040533a666ac2
TawhidulIslam0 <islam100tawhidul@gmail.com>
2026-03-10 17:41:07 -0400

    feat: add fileService.js for file upload API calls

---
+++ frontend/src/api/fileService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8000";
+
+export const uploadFile = async (file) => {
+  const formData = new FormData();
+
+  formData.append("file", file);
+
+  const token = localStorage.getItem("token");
+
+  const response = await axios.post(
+    `${API_URL}/files/upload`,
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return response.data;
+};
