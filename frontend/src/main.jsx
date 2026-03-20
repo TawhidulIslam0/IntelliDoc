0a0d3d03d63105baffa9b535a1a741ba068134d6
TawhidulIslam0 <islam100tawhidul@gmail.com>
2026-03-20 00:17:43 -0400

    feat: wrap App with ProfileProvider to enable global profile context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ProfileProvider } from "./UI/ProfileContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ProfileProvider>
+        <App />
+      </ProfileProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
