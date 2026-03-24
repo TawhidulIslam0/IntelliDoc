80b29f000d24082476491c5a2992a32542fbf28a
TawhidulIslam0 <islam100tawhidul@gmail.com>
2026-03-23 22:12:04 -0400

    feat: re-adding database imports

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,8 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
-
+from app.database import Base, engine
+from app.models import user, folder, file, profile
 from app.api.users import router as users_router
 from app.api.files import router as files_router
 from app.api.folders import router as folders_router
