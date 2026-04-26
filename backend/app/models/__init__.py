96e616f39a3d5b9d6b708ccbae8c5ca5c7211cec
TawhidulIslam0 <islam100tawhidul@gmail.com>
2026-04-25 23:00:06 -0400

    feat: export UploadChunk model in package

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -4,5 +4,6 @@ from .file import File
 from .profile import Profile  
 from .tab import Tab
 from .chunk import Chunk
+from .upload_chunk import UploadChunk
 
-__all__ = ["User", "Folder", "File", "Profile", "Tab", "Chunk"] 
+__all__ = ["User", "Folder", "File", "Profile", "Tab", "Chunk", "UploadChunk"] 
